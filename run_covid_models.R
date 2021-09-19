81abdb3b6568f18f31fce449ea98da9154dc4ac8
Cameron Patrick <cameron@largestprime.net>
2021-09-19 11:10:36 +1000

    wrapper script to run all scripts (TODO: learn {targets})

---
+++ run_covid_models.R
@@ -0,0 +1,19 @@
+
+library(here)
+library(glue)
+
+rmarkdown::render(here("covid_daily_download.Rmd"),
+                  "html_document",
+                  here("covid_daily_download.html"))
+rmarkdown::render(here("covid_epinow.Rmd"),
+                  "html_document",
+                  here("covid_epinow.html"))
+rmarkdown::render(here("covid_epinow_plots.Rmd"),
+                  "html_document",
+                  here("covid_epinow_plots.html"))
+fs::copy(here("covid_epinow_plots.html"),
+         here("html", glue("covid_epinow_plots_{Sys.Date()}.html")))
+fs::copy(here("covid_epinow_plots.html"),
+         here("docs", "index.html"),
+         overwrite = TRUE)
+
