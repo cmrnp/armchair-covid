c69c4bc50fd5ef8f5832bde5e7a659b630cfe98f
Cameron Patrick <cameron@largestprime.net>
2021-10-05 13:29:42 +1100

    update code for new directory layout

--- src/run_covid_models.R
+++ src/run_covid_models.R
@@ -2,19 +2,22 @@
 library(here)
 library(glue)
 
-rmarkdown::render(here("covid_daily_download.Rmd"),
-                  "html_document",
-                  here("covid_daily_download.html"))
-rmarkdown::render(here("covid_epinow.Rmd"),
-                  "html_document",
-                  here("covid_epinow.html"))
-rmarkdown::render(here("covid_epinow_plots.Rmd"),
-                  "html_document",
-                  here("covid_epinow_plots.html"))
-fs::file_copy(here("covid_epinow_plots.html"),
-              here("html", glue("covid_epinow_plots_{Sys.Date()}.html")),
-              overwrite = TRUE)
-fs::file_copy(here("covid_epinow_plots.html"),
-              here("docs", "index.html"),
-              overwrite = TRUE)
+do_render <- function(base, date = Sys.Date(), additional_output = NULL) {
+  src_file <- here("src", glue("{base}.Rmd"))
+  output_file <- here("output", "html", glue("{base}_{date}.html"))
+  rmarkdown::render(
+    input = src_file,
+    output_format = "html_document",
+    output_file = output_file
+  )
+  if (!is.null(additional_output)) {
+    fs::file_copy(output_file,
+                  additional_output,
+                  overwrite = TRUE)
+  }
+}
 
+do_render("covid_daily_download")
+do_render("covid_epinow")
+do_render("covid_epinow_plots", 
+          additional_output = here("docs", "index.html"))
